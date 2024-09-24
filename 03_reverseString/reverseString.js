802e2eb5260817825732cd86c6d1a91a78466a3c
BabulalxPy <babulalchaudhari8329@gmail.com>
2024-09-24 11:50:06 +0530

    Exercise 3 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let string = "";
+    for(var i = word.length-1;i>=0;i--){
+        string += word[i];
+    }
+    return string;
+    //optional method: 
+    //string = word.split("").reverse().join("");
+    //return string;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
