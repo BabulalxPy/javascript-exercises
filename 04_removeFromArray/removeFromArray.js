50450cd02474bf1b24b5c8b0f95fb1a77b409c90
BabulalxPy <babulalchaudhari8329@gmail.com>
2024-09-24 18:18:26 +0530

    complete exercise 4(it was hard, i am not satisfied)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
-};
-
+//my solution incomplete
+//const removeFromArray = function(array, ...args) {
+  //  args.forEach((arg) =>{
+    //    uniq = array.filter((item, index) => array.indexOf(item) === index);
+      //  let index = uniq.indexOf(arg);
+        //if(index > -1){
+      //  uniq.splice(index,1);
+        //}
+    //});
+    
+    //return uniq;
+//};
+//console.log(removeFromArray([1,4,,3,4,4,5],5,4))
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
+
+const removeFromArray = function (array, ...args) {
+    // create a new empty array
+    const newArray = [];
+    // use forEach to go through the array
+    array.forEach((item) => {
+      // push every element into the new array
+      // UNLESS it is included in the function arguments
+      // so we create a new array with every item, except those that should be removed
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    // and return that array
+    return newArray;
+  };
+  module.exports = removeFromArray;
