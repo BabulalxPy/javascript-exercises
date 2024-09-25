279cc2f1af31c3671c83bcbc0f984253cf676d99
BabulalxPy <babulalchaudhari8329@gmail.com>
2024-09-25 21:57:57 +0530

    Exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(numOne,numTwo) {
+    sum = 0;
+    if(numOne<numTwo && numOne >= 0 && numTwo >= 0 && numOne == Math.floor(numOne) && numTwo == Math.floor(numTwo) && typeof(numOne) == "number" && typeof(numTwo) == "number"){
+        for(let i = numOne; i <=numTwo; i++){
+            sum += i;
+        }
+    }
+    else if(numOne>numTwo && numOne >= 0 && numTwo >= 0 && numOne == Math.floor(numOne) && numTwo == Math.floor(numTwo) && typeof(numOne) == "number" && typeof(numTwo) == "number"){
+        for(let i = numOne; i >=numTwo; i--){
+            sum += i;
+        }
+    }
+    else{
+        sum = "ERROR";
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
