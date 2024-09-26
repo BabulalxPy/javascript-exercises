8895f9c8d3a924098bcfb539d65434baaf12dbcb
BabulalxPy <babulalchaudhari8329@gmail.com>
2024-09-26 15:40:46 +0530

    Exercise 07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp-32)*(5/9);
+  let roundedCelsius = Math.round(celsius*10)/10;
+  return roundedCelsius;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  let fahrneheit = (temp2*(9/5)+32);
+  let roundedFahrenheit = Math.round(fahrneheit*10)/10;
+  return roundedFahrenheit;
 };
-
+//console.log(convertToCelsius(100))
+//console.log(convertToFahrenheit(100))
 // Do not edit below this line
-module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
+module.exports = {convertToCelsius, convertToFahrenheit };
