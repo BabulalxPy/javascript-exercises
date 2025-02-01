8c9fa6f7519fb2ac1aaf25013eead1f2e0b42650
BabulalxPy <babulalchaudhari8329@gmail.com>
2025-02-01 19:31:06 +0530

    palindrome exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function(word){
+    let newWord = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replaceAll(" ","").toLowerCase();
+    let rev = newWord.split('').reverse().join('');
+    let newRev = rev.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replaceAll(" ","").toLowerCase();
+    if(newWord === newRev){
+        return true;
+    }else{
+        return false;
+    }
 
 };
-
+console.log(palindromes("RE, R..,E R"));
 // Do not edit below this line
 module.exports = palindromes;
