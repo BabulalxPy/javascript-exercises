922c4da110712063465db8eb35980f52f358c6f4
BabulalxPy <babulalchaudhari8329@gmail.com>
2025-02-26 17:42:13 +0530

    Fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    if(num<0){
+        return "OOPS";
+    }
+    else if(num == 0){
+        return num1;
+    }
+    else if(num == 1){
+        return num2;
+    }else{
+        for(let i = 2; i <= num; i++){
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return sum;
+    }
 };
-
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
