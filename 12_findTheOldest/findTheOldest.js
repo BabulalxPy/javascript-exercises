fae9ea8bfbb785cd662309e89580e7dd8c9b2078
BabulalxPy <babulalchaudhari8329@gmail.com>
2025-02-26 20:21:49 +0530

    complete the findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
 
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, youngest) =>{
+        const getOldest = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const getyoungest = getAge(youngest.yearOfBirth, youngest.yearOfDeath);
+        return getOldest < getyoungest ? youngest : oldest;
+    });
 };
 
 // Do not edit below this line
